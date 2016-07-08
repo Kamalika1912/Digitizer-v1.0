1ce644271c3a2a3bbde1276a264702fda32521e6
Kamalika Dutta <kamalika1912@gmail.com>
2016-07-08 20:46:12 +0200

    interface improved

--- src/app/modules/project/edit/edit.js
+++ src/app/modules/project/edit/edit.js
@@ -1,25 +1,13 @@
 (function() {
   'use strict';
-
-  angular
-    .module('app.profile')
-    .controller('ProjectController', projectController);
-
+  angular.module('app.profile').controller('ProjectController', projectController);
   projectController.$inject = ['projects', 'projectResource', 'elements', '$state', 'shortHistory', 'notificator'];
+
   function projectController(projects, projectResource, elements, $state, shortHistory, notificator) {
     var vm = this;
     vm.project = projects;
     vm.element = elements;
     vm.showReturnBtn = vm.project.id && shortHistory.from.state.name;
-    for (var projectKey in vm.project){
-      for (var serviceKey in vm.project[projectKey].services) {
-
-        console.log(vm.project[projectKey].services[serviceKey].id);
-
-      }
-    }
-
-
     vm.update = function() {
       vm.project.date = (new Date()).toISOString();
       projectResource.update(vm.project, function(p) {
@@ -27,15 +15,12 @@
         notificator.success('Project was successfully updated')
       });
     };
-
     vm.return = function() {
-        $state.go(shortHistory.from.state.name, shortHistory.from.params);
+      $state.go(shortHistory.from.state.name, shortHistory.from.params);
     };
-
     vm.reset = function() {
       $state.reload();
     };
-
     vm.save = function() {
       vm.project.date = (new Date()).toISOString();
       projectResource.save(this.project, function(savedProject) {
@@ -43,9 +28,5 @@
         notificator.success('Project was successfully saved')
       });
     };
-
-
-
   }
-
 })();
