f1f276eb582f21320a621d186a6b8b082275832f
Kamalika Dutta <kamalika1912@gmail.com>
2016-08-19 00:21:29 +0200

    implemented mapping of services for each activity separately

--- src/app/modules/project/digitize/digitize.js
+++ src/app/modules/project/digitize/digitize.js
@@ -9,7 +9,33 @@
     vm.activity = activities;
     vm.services = digitalServices;
     vm.showReturnBtn = vm.project.id && shortHistory.from.state.name;
-    vm.suggestedServices = vm.services;
+    var suggestedServices = [];
+    //vm.suggestedServices = vm.services;
+    for (var activityKey in vm.project.activities){
+      suggestedServices.push({});
+    }
+    console.log(suggestedServices);
+
+    for (var activityKey in vm.project.activities){
+      suggestedServices[activityKey] = vm.services.filter(function(eachService) {
+        for (var serviceKey in eachService.tags) {
+            for (var tagKey in vm.project.activities[activityKey].tags) {
+            //console.log(eachService.tags[serviceKey].text);
+              if (eachService.tags[serviceKey].text === vm.project.activities[activityKey].tags[tagKey].text){
+                return true;
+              }
+            }
+        }
+        return false;
+      });
+      console.log(suggestedServices[activityKey]);
+    }
+    for (var serviceKey in suggestedServices) {
+      for (var tagKey in suggestedServices.tags) {
+        console.log(suggestedServices[serviceKey].tags[tagKey].text);
+      }
+    }
+    /*
     vm.suggestedServices = vm.services.filter(function(eachService) {
 
       for (var serviceKey in eachService.tags) {
@@ -25,6 +51,9 @@
       }
       return false;
     });
+
+    */
+
     vm.undo = function() {
       vm.project.services.splice(-1,1);
     };
