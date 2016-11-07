eefdf290c1c0d344264c73736169788e1ff9fefe
Kamalika Dutta <kamalika1912@gmail.com>
2016-11-07 12:09:56 +0100

    minor update

--- src/app/modules/project/digitize/digitize.js
+++ src/app/modules/project/digitize/digitize.js
@@ -11,25 +11,31 @@
     vm.showReturnBtn = vm.project.id && shortHistory.from.state.name;
     vm.suggestedServices = [];
     //vm.suggestedServices = vm.services;
-    for (var activityKey in vm.project.activities){
+    for (var activityKey in vm.project.activities) {
       vm.suggestedServices.push({});
     }
 
     if (vm.project.services == []) {
       for (var activityKey in vm.project.activities) {
-        vm.project.services.push(' ');
+        //vm.project.services.push(' ');
+        vm.project.services.push({});
       }
     }
-    console.log(vm.project.services);
-    for (var activityKey in vm.project.activities){
+    console.log('services: ' + vm.project.services);
+    for (var serviceKey in vm.project.services) {
+      for (var tagKey in vm.project.services.tags) {
+        console.log(vm.project.service[serviceKey].tags[tagKey].text);
+      }
+    }
+   for (var activityKey in vm.project.activities) {
       vm.suggestedServices[activityKey] = vm.services.filter(function(eachService) {
         for (var serviceKey in eachService.tags) {
-            for (var tagKey in vm.project.activities[activityKey].tags) {
+          for (var tagKey in vm.project.activities[activityKey].tags) {
             //console.log(eachService.tags[serviceKey].text);
-              if (eachService.tags[serviceKey].text === vm.project.activities[activityKey].tags[tagKey].text || eachService.tags[serviceKey].text == 'no service'){
-                return true;
-              }
+            if (eachService.tags[serviceKey].text === vm.project.activities[activityKey].tags[tagKey].text || eachService.tags[serviceKey].text == 'no service') {
+              return true;
             }
+          }
         }
         return false;
       });
@@ -37,33 +43,33 @@
       //console.log(vm.suggestedServices[activityKey]);
       //console.log(vm.services[0]);
     }
-    //console.log(vm.suggestedServices);
-    for (var serviceKey in vm.suggestedServices) {
-      for (var tagKey in vm.suggestedServices.tags) {
-        console.log(vm.suggestedServices[serviceKey].tags[tagKey].text);
-      }
-    }
+    /*    //console.log(vm.suggestedServices);
+     for (var serviceKey in vm.suggestedServices) {
+     for (var tagKey in vm.suggestedServices.tags) {
+     console.log(vm.suggestedServices[serviceKey].tags[tagKey].text);
+     }
+     }*/
     /*
-    vm.suggestedServices = vm.services.filter(function(eachService) {
+     vm.suggestedServices = vm.services.filter(function(eachService) {
 
-      for (var serviceKey in eachService.tags) {
-        for (var activityKey in vm.project.activities){
-          for (var tagKey in vm.project.activities[activityKey].tags) {
-            //console.log(vm.project.activities[activityKey].tags[tagKey].text);
-            if (eachService.tags[serviceKey].text === vm.project.activities[activityKey].tags[tagKey].text){
-              return true;
-            }
-          }
-        }
+     for (var serviceKey in eachService.tags) {
+     for (var activityKey in vm.project.activities){
+     for (var tagKey in vm.project.activities[activityKey].tags) {
+     //console.log(vm.project.activities[activityKey].tags[tagKey].text);
+     if (eachService.tags[serviceKey].text === vm.project.activities[activityKey].tags[tagKey].text){
+     return true;
+     }
+     }
+     }
 
-      }
-      return false;
-    });
+     }
+     return false;
+     });
 
-    */
+     */
 
     vm.undo = function() {
-      vm.project.services.splice(-1,1);
+      vm.project.services.splice(-1, 1);
     };
     vm.refresh = function() {
       $state.reload();
@@ -75,12 +81,11 @@
 
     vm.update = function() {
       vm.project.date = (new Date()).toISOString();
-      if (vm.project.services.length <= vm.project.activities.length){
+      if (vm.project.services.length <= vm.project.activities.length) {
         projectResource.update(vm.project, function(p) {
           notificator.success('Digitized Model was successfully saved');
         });
-      }
-      else (
+      } else (
         notificator.warning('Services exceeding model activities')
       )
     };
